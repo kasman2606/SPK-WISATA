c2970f7b9f24f13e3bbf87b6c503d0e5697c5f2f
kasman2606 <kasmancoding@gmail.com>
2023-11-06 08:56:54 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php require "layout/head.php";?>
+
+    <body>
+        <div id="app">
+            <?php require "layout/sidebar.php";?>
+            <div id="main">
+                <header class="mb-3">
+                    <a href="#" class="burger-btn d-block d-xl-none">
+                        <i class="bi bi-justify fs-3"></i>
+                    </a>
+                </header>
+                <div class="page-heading">
+                    <h3>Selamat Datang Di Aplikasi Sistem Pendukung Keputusan</h3>
+                </div>
+                <div class="page-content">
+                    <section class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4></h4>
+                                </div>
+                                <div class="card-content">
+                                    <div class="card-body">
+                                        <p class="card-text">
+                                          
+                                        </p>
+                                        <hr>
+                                        <p class="card-text">
+                                           
+                                        </p>
+                                        <ol type="1">
+                                            
+                                        </ol>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+                <?php require "layout/footer.php";?>
+            </div>
+        </div>
+        <?php require "layout/js.php";?>
+    </body>
+
+</html>
